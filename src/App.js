d25f9e0dcc5e14299d44e131b6fb0dc7c8b4d393
bhavyPatel13 <frontentdeveloper853@gmail.com>
2025-04-23 13:43:29 +0530

    useContext hook: Using React Context API

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ import {
 import Navbar from './component/Navbar';
 import Home from './component/Home';
 import About from './component/About';
+import NoteState from './context/notes/noteState';
 
 function App() {
   return (
     <>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-        </Routes>
-      </Router>
+      <NoteState>
+        <Router>
+          <Navbar/>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+        </Router>
+      </NoteState>
     </>
   );
 }
