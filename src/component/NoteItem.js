4cee50a33b1132436d627d5e93594d978cd75478
bhavyPatel13 <frontentdeveloper853@gmail.com>
2025-04-25 13:14:10 +0530

    Adding delete note functionality to iNotebook

--- src/component/NoteItem.js
+++ src/component/NoteItem.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { useContext } from 'react';
+import ContextValue from '../context/notes/noteContext';
 
 const NoteItem = (props) => {
-    const {note} = props;
+    const context = useContext(ContextValue);
+    const { deleteNote } = context;
+    const { note } = props;
     return (
         <div className="col-md-3">
             <div className="card">
                 <div className="card-body">
                     <h5 className="card-title">{note.title}</h5>
                     <p className="card-text">{note.description}</p>
-                    <i className="fa-solid fa-trash mx-2"></i>
+                    <i className="far fa-trash-alt mx-2" onClick={() => {deleteNote(note._id)}} ></i>
                     <i className="far fa-edit mx-2"></i>
                 </div>
             </div>
